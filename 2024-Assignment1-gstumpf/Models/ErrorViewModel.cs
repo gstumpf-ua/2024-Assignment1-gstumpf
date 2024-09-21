8db4f15ac82b5005b44a1f6a0aca06935b5ac243
gavin <gavin@DESKTOP-RQI62A3>
2024-09-20 21:54:53 -0500

    Add project files.

---
+++ 2024-Assignment1-gstumpf/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _2024_Assignment1_gstumpf.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
